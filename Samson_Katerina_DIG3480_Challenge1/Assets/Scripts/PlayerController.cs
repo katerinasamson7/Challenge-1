0e87a54c95091b6dad55cad23bdca33f1ae25d9b
Katerina Samson <54958992+katerinasamson7@users.noreply.github.com>
2019-09-22 22:03:21 -0400

    Challenge 1

---
+++ Samson_Katerina_DIG3480_Challenge1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        SetCountText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count == 12)
+        {
+            transform.position = new Vector2(-62.2f, 54.4f);
+        }
+
+        if (count >= 20)
+        {
+            winText.text = "You win! Game created by Katerina Samson!";
+        }
+
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives <= 0)
+        {
+            winText.text = "Game Over!";
+            Destroy(gameObject);
+        }
+    }
+}
